00caba010a515bcd714d6bfd7a01b992ad2798b1
NITHIKESH <nithikesh.b2019@vitstudent.ac.in>
2025-02-09 15:51:48 +0530

    updated sequelize connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,9 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
+  dialectOptions: {
+    connectTimeout: 60000 // Increase to 60 seconds
+  },
   host: process.env.DB_HOST || 'localhost',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
